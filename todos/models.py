3489c7c00fddfc53435de5eb2903b1144fa923a9
Ava Wallenfang <s6mmwall@uni-bonn.de>
2024-07-26 21:03:50 +0200

    Move variables into base.css and add todo models

--- todos/models.py
+++ todos/models.py
@@ -2,4 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    pass
+    name = models.CharField(max_length=500, blank=False, null=False)
+    description = models.CharField(max_length=2000, blank=False, null=False)
+    def __str__(self):
+        return f'{self.name}: {self.description}'
+
+class SubTask(models.Model):
+    title = models.CharField(max_length=500, blank=False, null=False)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'SubTask: {self.title}'
+
+class SubTaskToTodo(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    subtask = models.ForeignKey(SubTask, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.todo.name} - {self.subtask.title}'

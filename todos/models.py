8c312635732959d144c6912e101aa7606fc69b7f
Ava Wallenfang <s6mmwall@uni-bonn.de>
2024-09-11 18:20:06 +0200

    Added models for recurrency and user assignment

--- todos/models.py
+++ todos/models.py
@@ -3,6 +3,33 @@ from django.db.models import F
 
 from hub.models import SharedSpace, User
 
+######## Recurrent Todo helpers
+
+class _OrderedUser(models.Model):
+    user = models.ForeignKey("hub.User")
+    recurrent_todo = models.ForeignKey("todos.TodoRecurrency")
+    order = models.IntegerField(default=0)
+
+class _TodoRecurrency(models.Model):
+    assigned_users = models.ManyToManyField("hub.User", through=_OrderedUser)
+    recurrency_turn = models.IntegerField(default=0, blank=False, null=False)
+    started_at = models.DateField(auto_created=True)
+    day_rotation = models.IntegerField(default=7)
+
+    def create_with_settings(users, rate):
+        recurrency = _TodoRecurrency.objects.create(recurrency_turn = 0, day_rotation = rate)
+
+        for (i, user) in enumerate(users):
+            _OrderedUser(user=user, recurrent_todo = recurrency, order = i)
+            _OrderedUser.save()
+
+        return recurrency
+    
+    def add_user(self,user):
+        _OrderedUser(user=user, recurrent_todo = self, order = len(_OrderedUser.objects.all(recurrent_todo = self)))
+        _OrderedUser.save()
+
+#######
 
 class Todo(models.Model):
     name = models.CharField(max_length=500, blank=False, null=False)
@@ -10,6 +37,8 @@ class Todo(models.Model):
     done = models.BooleanField(default=False)
     position = models.IntegerField()
     space = models.ForeignKey(SharedSpace, on_delete=models.CASCADE)
+    recurrent_state = models.ForeignKey(_TodoRecurrency, on_delete=models.CASCADE, blank=True, null=True)
+    assigned_user = models.ForeignKey("hub.User", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f'{self.name}: {self.description} | Position: {self.position} | Done: {self.done}'
@@ -24,8 +53,6 @@ class Todo(models.Model):
             Todo.minimize_positions()
             max_pos = Todo.objects.all().order_by('-position')[0].position
         Todo.objects.create(name="New Todo", description = "", position = max_pos+1, space=space)
-
-        
 
     def get_open(request):
         user = User.objects.get(auth_user = request.user)
@@ -50,6 +77,9 @@ class Todo(models.Model):
 
     @transaction.atomic
     def reorder(self, left: int, right: int):
+        """
+        Reorder the todos in the overview. Left and right are the positions of the todos at those spaces. -1 is used if there is no todo there
+        """
         # Left border
         if left == -1:
             self.position = int(right)
@@ -72,6 +102,37 @@ class Todo(models.Model):
         self.save()
 
         Todo.minimize_positions()
+        
+    def assign_user(self, user):
+        """
+        Assign a user to a todo that isn't recurrent
+        """
+        recurrency = self.recurrent_state
+        if not recurrency:
+            self.assigned_user = user
+            self.save()
+
+    def make_recurrent(self, users=[], rate=7):
+        """
+        Turn the todo into a recurrent todo with the specified users and the specified rate.
+        The users are ordered
+        """
+        recurrency = _TodoRecurrency.create_with_settings(users, rate)
+
+        self.recurrent_state = recurrency
+        self.save()
+
+    def add_recurrent_user(self, user):
+        """
+        Add a user to the recurrent todo
+        """
+        recurrency = self.recurrent_state
+        if recurrency:
+            recurrency.add_user(user)
+            recurrency.save()
+
+    
+
 
 class SubTask(models.Model):
     title = models.CharField(max_length=500, blank=False, null=False)
@@ -80,3 +141,4 @@ class SubTask(models.Model):
 
     def __str__(self):
         return f'SubTask: {self.title} on {self.todo.name}'
+    

ea9bc930c6effcc950b30b921c300bd0299185d6
awallenfang <marvin@wallenfang.de>
2025-01-05 19:43:13 +0100

    Lock features for spaces

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,6 @@
+from functools import wraps
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404

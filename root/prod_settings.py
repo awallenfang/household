e8fd7fcfe8ef578a1767f7c13646a69f835cc452
root <root@ritzin.dev>
2025-05-27 05:24:51 +0000

    database has no need to be in the network

--- root/prod_settings.py
+++ root/prod_settings.py
@@ -12,3 +12,15 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 ALLOWED_HOSTS = [
     "web"
 ]
+
+DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': os.environ.get("POSTGRES_DB"),
+                'USER': os.environ.get("POSTGRES_USER"),
+                'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+                'HOST': os.environ.get("POSTGRES_HOST"),
+                'PORT': os.environ.get("POSTGRES_PORT"),
+            }
+        }
+

b81b48407e1ecb13297cdb3d3c5c23dde02544f2
awallenfang <marvin@wallenfang.de>
2025-05-30 22:26:11 +0200

    Fix translations

--- hub/forms.py
+++ hub/forms.py
@@ -1,17 +1,17 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 
 class LoginForm(forms.Form):
     template_name = "hub/form_template.html"
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    username = forms.CharField(label=_("Username"), max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
 class SignupForm(forms.Form):
     template_name = "hub/form_template.html"
-    username = forms.CharField(max_length=100, 
+    username = forms.CharField(label=_("Username"), max_length=100, 
                                widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'E-Mail'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    repeat_password = forms.CharField(widget=forms.PasswordInput(
+    email = forms.EmailField(label=_("Email Address"), widget=forms.TextInput(attrs={'placeholder': 'E-Mail'}))
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    repeat_password = forms.CharField(label=_("Repeat password"), widget=forms.PasswordInput(
                                                     attrs={'placeholder': 'Repeat Password'}
                                                     ))

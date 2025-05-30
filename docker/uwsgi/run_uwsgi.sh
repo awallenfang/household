a7a8a1965d2d40ada8cb24cd55e8fe9d3db7271a
awallenfang <marvin@wallenfang.de>
2025-05-30 21:10:09 +0200

    Add locale compile script

--- docker/uwsgi/run_uwsgi.sh
+++ docker/uwsgi/run_uwsgi.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 python manage.py migrate
 python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME --noinput
+cd hub & python ../manage.py compilemessages & cd ..
+cd todos & python ../manage.py compilemessages & cd ..
+cd space & python ../manage.py compilemessages & cd ..
 
 uwsgi --ini /code/docker/uwsgi/uwsgi.ini

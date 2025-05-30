17258843933a450caab7ff3e42552f178ed30d99
awallenfang <marvin@wallenfang.de>
2025-05-30 10:38:23 +0200

    Fix edit leaks

--- todos/forms.py
+++ todos/forms.py
@@ -9,6 +9,8 @@ class TodoForm(forms.ModelForm):
         model = Todo
         fields = ["description", "name", "assigned_user"]
         
-    def __init__(self, *args, **kwargs):
+    def __init__(self, space=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["description"].required = False
+        if space:
+            self.fields["assigned_user"].queryset = Profile.objects.filter(spaces__in = [space])
--- todos/views.py
+++ todos/views.py
@@ -104,8 +104,10 @@ class EditTodoView(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        profile = Profile.objects.get(user = self.request.user)
+
         # For some reason this doesn't populate the form properly
-        context["form"] = self.form_class(instance=self.get_object())
+        context["form"] = self.form_class(profile.selected_space, instance=self.get_object())
         return context
     
     def form_valid(self, form):

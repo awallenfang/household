38c1c24a6533fdee7d445903dabcce6efe2b1909
awallenfang <marvin@wallenfang.de>
2025-06-08 17:01:45 +0200

    Update docker container to compile scss

--- docker/uwsgi/run_uwsgi.sh
+++ docker/uwsgi/run_uwsgi.sh
@@ -2,5 +2,8 @@
 python manage.py migrate
 python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME --noinput
 python manage.py compilemessages -v0
+python manage.py sass /code/hub/static/hub/scss /code/hub/static/hub/css -t compressed
+python manage.py sass /code/todos/static/todos/scss /code/todos/static/todos/css -t compressed
+python manage.py sass /code/space/static/space/scss /code/space/static/space/css -t compressed
 
 uwsgi --ini /code/docker/uwsgi/uwsgi.ini

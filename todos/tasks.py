46a28ea3115add990e7e0877274d85a868937df8
awallenfang <marvin@wallenfang.de>
2025-07-19 15:40:32 +0200

    idk anymore

--- todos/tasks.py
+++ todos/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
 
 from todos.models import Todo
-
+import logging
 @shared_task
 def update_todo_schedules():
+    logging.info("RUNNING")
     Todo.check_schedule_update()

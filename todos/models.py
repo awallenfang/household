b2dfb3218f7861004988ba314733e7110b700848
awallenfang <marvin@wallenfang.de>
2025-07-19 15:47:23 +0200

    aaa

--- todos/models.py
+++ todos/models.py
@@ -159,6 +159,7 @@ class TodoSchedule(models.Model):
         last_date = self.last_check
 
         day_difference = (date_now - last_date).days
+        self.schedule_turn += 1 % len(self.assigned_users.all())
         if day_difference > 0:
             old_turn = self.schedule_turn
             self.schedule_turn += day_difference % len(self.assigned_users.all())
